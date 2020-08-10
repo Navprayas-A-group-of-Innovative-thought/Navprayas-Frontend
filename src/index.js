6d62af0921c07e848dff6ae8d5109005b52678b4
Aman <amanloc1@hotmail.com>
2020-08-10 18:44:40 +0530

    remove redundant code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import store from './/store';
 ReactDOM.render(
   <React.StrictMode>
     <App />
