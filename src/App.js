372f6f3abc12b57145b14a71c1b85b05fb40d6f7
Akash Kumar <aksh.iem22@gmail.com>
2020-08-10 17:01:50 +0530

    Added basic instructions and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./styles/App.module.css";
+import Sample from "./components/sample.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <img src={logo} className={styles.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Sample />
       </header>
     </div>
   );
