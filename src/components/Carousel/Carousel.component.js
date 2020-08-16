44f46b250f3f5166da9774d782668b245da8b249
muskan89 <muskangupta1107@gmail.com>
2020-08-17 00:58:54 +0530

    Carousel for home screen

--- src/components/Carousel/Carousel.component.js
+++ src/components/Carousel/Carousel.component.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
-function Carousel() {
-    return(
-        <div className="item" id="Carousel"></div>
-    )
+const items = [
+  {
+    src: 'https://user-images.githubusercontent.com/48627031/90132130-dfe87400-dd8a-11ea-9ab5-7fdb5d79ada8.jpg',
+    altText: 'Slide 1',
+    caption: 'Pratibha Milan'
+  },
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Rectangle_example.svg',
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: 'https://maxwelldemon.files.wordpress.com/2012/03/2x1-rectangle.png',
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+];
+
+const Carousell = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
 }
-export default Carousel
+
+export default Carousell;
+
