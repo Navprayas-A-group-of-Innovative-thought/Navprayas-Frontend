dfe446c773d6b6b4a0773a90970d797751cf2187
22Rahul22 <rahulheritage22@gmail.com>
2020-08-20 14:27:06 +0530

    Added Sign-up page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home";
 import Event from "./pages/Event";
+import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/events" component={Event} />
+          <Route exact path = "/signUp" component={SignUp} />
         </Switch>
         <Footer />
       </BrowserRouter>
