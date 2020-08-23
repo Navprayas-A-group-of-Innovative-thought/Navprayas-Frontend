4f6616a3dbb975806730540508bfa48848a513dc
Akash Kumar <aksh.iem22@gmail.com>
2020-08-23 21:43:32 +0530

    Added redirects to home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 import Home from "./pages/home";
 import Event from "./pages/Event";
-import LoginRegister from "./pages/Login-Register"
+import LoginRegister from "./pages/Login-Register";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
 
 function App() {
   return (
@@ -14,9 +13,22 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/events" component={Event} />
-          <Route exact path="/login" component={() => <LoginRegister title="Login" formType="login" /> } />
-          <Route exact path="/signup" component={() => <LoginRegister title="Create New Account" formType="signup" />} />
+          {
+            //<Route exact path="/events" component={Event} />}
+          }
+          <Route
+            exact
+            path="/login"
+            component={() => <LoginRegister title="Login" formType="login" />}
+          />
+          <Route
+            exact
+            path="/signup"
+            component={() => (
+              <LoginRegister title="Create New Account" formType="signup" />
+            )}
+          />
+          <Redirect to="/" />
         </Switch>
         <Footer />
       </BrowserRouter>
