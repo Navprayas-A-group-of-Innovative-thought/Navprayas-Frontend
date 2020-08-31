373bb760e1bb34a00a6d89d65f8f636e0d0cad25
Akash Kumar <aksh.iem22@gmail.com>
2020-09-01 02:58:05 +0530

    add faq section

---
+++ src/components/Timeline/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./timeline.css";
+
+const Timeline = () => {
+  return (
+    <section className="timeline">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center title">
+            <h2>EVENT SCHEDULE</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Timeline;
