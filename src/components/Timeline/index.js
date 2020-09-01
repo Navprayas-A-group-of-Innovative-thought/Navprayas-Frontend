517b56fa3c9d2a0f58f7280dfe17d76e9e0e2ae6
Rahul <rahulbhl22@gmail.com>
2020-09-02 00:09:37 +0530

    Add timeline section

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./timeline.css";
+import HorizontalTimeline from 'react-horizontal-timeline';
+import Content from './content';
+import timelineContent from './timelineContent';
+
+const VALUES = timelineContent.VALUES;
+const Contents = timelineContent.Contents;
 
 const Timeline = () => {
+
+  const [value, setvalue] = useState(0);
+  const [previous, setprevious] = useState(0)
+
   return (
-    <section className="timeline">
-      <div className="container">
-        <div className="row">
-          <div className="col text-center title">
-            <h2>EVENT SCHEDULE</h2>
-          </div>
+    <section style={{height:"500px"}}>
+      <div className="container text-center">
+        <h1 className="pt-5">Event Schedule</h1>
+        <div style={{ width: '70%', height: '100px', margin: '30px auto', fontSize: "14px" }}>
+          <HorizontalTimeline
+            minEventPadding="76"
+            maxEventPadding="76"
+            LabelWidth="100"
+            styles={{ background: '#fff', foreground: '#F69925', outline: '#dfdfdf' }}
+            fillingMotion={{ stiffness: "150", damping: "25" }}
+            slidingMotion={{ stiffness: "150", damping: "25" }}
+            index={value}
+            indexClick={(index) => {
+              setvalue(index);
+              setprevious(value);
+            }}
+            values={VALUES} />
+        </div>
+        <div style={{ textAlign: "center" }} className="text-center">
+          <Content contents={Contents} values={value} />
         </div>
       </div>
     </section>
