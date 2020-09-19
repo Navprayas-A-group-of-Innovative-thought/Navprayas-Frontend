d57f1f3b01286a7138749ff7d6f3af8ed28f7d01
Akash Kumar <aksh.iem22@gmail.com>
2020-09-19 23:49:38 +0530

    payement form

--- src/redux/actions/payement.actions.js
+++ src/redux/actions/payement.actions.js
@@ -7,46 +7,46 @@ export const payementActions = {
   pay,
 };
 
-// function isDate(val) {
-//   // Cross realm comptatible
-//   return Object.prototype.toString.call(val) === "[object Date]";
-// }
+function isDate(val) {
+  // Cross realm comptatible
+  return Object.prototype.toString.call(val) === "[object Date]";
+}
 
-// function isObj(val) {
-//   return typeof val === "object";
-// }
+function isObj(val) {
+  return typeof val === "object";
+}
 
-// function stringifyValue(val) {
-//   if (isObj(val) && !isDate(val)) {
-//     return JSON.stringify(val);
-//   } else {
-//     return val;
-//   }
-// }
+function stringifyValue(val) {
+  if (isObj(val) && !isDate(val)) {
+    return JSON.stringify(val);
+  } else {
+    return val;
+  }
+}
 
-// function buildForm({ action, params }) {
-//   const form = document.createElement("form");
-//   form.setAttribute("method", "post");
-//   form.setAttribute("action", action);
-//   //   form.setAttribute("target", target);
+function buildForm({ action, params }) {
+  const form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", action);
+  //   form.setAttribute("target", target);
 
-//   Object.keys(params).forEach((key) => {
-//     const input = document.createElement("input");
-//     input.setAttribute("type", "hidden");
-//     input.setAttribute("name", key);
-//     input.setAttribute("value", stringifyValue(params[key]));
-//     form.appendChild(input);
-//   });
+  Object.keys(params).forEach((key) => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "hidden");
+    input.setAttribute("name", key);
+    input.setAttribute("value", stringifyValue(params[key]));
+    form.appendChild(input);
+  });
 
-//   return form;
-// }
+  return form;
+}
 
-// function post(details) {
-//   const form = buildForm(details);
-//   document.body.appendChild(form);
-//   form.submit();
-//   form.remove();
-// }
+function post(details) {
+  const form = buildForm(details);
+  document.body.appendChild(form);
+  form.submit();
+  form.remove();
+}
 
 function pay() {
   return (dispatch) => {
@@ -61,13 +61,13 @@ function pay() {
     axios
       .get(`${url}/paytm/payment?formId=MTSE`, config)
       .then((res) => {
-        console.log(res.data);
-        // var details = {
-        //   action: "https://securegw-stage.paytm.in/order/process",
-        //   params: res.data,
-        // };
+        console.log(res.data.data);
+        var details = {
+          action: `https://securegw-stage.paytm.in/theia/api/v1/showPaymentPage?mid=${res.data.data.mid}&orderId=${res.data.data.orderId}`,
+          params: res.data,
+        };
 
-        // post(details);
+        post(details);
       })
       .catch((err) => {
         console.log(err);
